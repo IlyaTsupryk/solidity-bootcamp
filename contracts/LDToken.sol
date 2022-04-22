bcb35b90b90269386aa37e772e0379ce19c829f0
Ilya Tsupryk <ilya_tsupryk@epam.com>
2022-04-22 10:20:56 +0300

    Implement transfer and approve methods

--- contracts/LDToken.sol
+++ contracts/LDToken.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.0;
  */
 contract LDToken {
     mapping(address => uint256) _balances;
+    mapping(address => mapping(address => uint)) _allowance;
 
     uint256 _totalSupply;
 
@@ -13,6 +14,7 @@ contract LDToken {
     string public _symbol;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
@@ -45,48 +47,30 @@ contract LDToken {
         return true;
     }
 
-    /**
-     * Transfers _value amount of tokens from address _from to address _to, and MUST fire the Transfer event.
-     *
-     * The transferFrom method is used for a withdraw workflow,
-     * allowing contracts to transfer tokens on your behalf.
-     * This can be used for example to allow a contract to transfer tokens on
-     * your behalf and/or to charge fees in sub-currencies.
-     * The function SHOULD throw unless the _from account has deliberately
-     * authorized the sender of the message via some mechanism.
-     *
-     * Note Transfers of 0 values MUST be treated as normal transfers and fire the Transfer event.
-     */
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _value
-    ) public returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_balances[_from] >= _value, "Not enough funds");
+        require(_allowance[_from][msg.sender] >= _value, "Tried to transfer more then allowed");
 
-    /**
-     * Allows _spender to withdraw from your account multiple times,
-     * up to the _value amount.
-     * If this function is called again it overwrites the current allowance with _value.
-     *
-     * NOTE: To prevent attack vectors like the one described here and discussed here,
-     * clients SHOULD make sure to create user interfaces in such a way
-     * that they set the allowance first to 0 before setting it to another value for the same spender.
-     * THOUGH The contract itself shouldn’t enforce it,
-     * to allow backwards compatibility with contracts deployed before.
-     */
-    function approve(address _spender, uint256 _value)
-        public
-        returns (bool success)
-    {}
+        _allowance[_from][msg.sender] -= _value;
+        _balances[_from] -= _value;
+        _balances[_to] += _value;
 
-    /**
-     * Returns the amount which _spender is still allowed to withdraw from _owner.
-     */
-    function allowance(address _owner, address _spender)
-        public
-        view
-        returns (uint256 remaining)
-    {}
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        _allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return _allowance[_owner][_spender];
+    }
 
     function _mint(address account, uint256 amount) external virtual {
         _balances[account] += amount;
